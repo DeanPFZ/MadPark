b333c085663952fe4285a1ccce53f4e712123655
Pengfei Zhu <pzhu6@wisc.edu>
2019-04-28 16:05:27 -0500

    Minor

--- app/src/main/java/com/example/madpark/MapsActivity.java
+++ app/src/main/java/com/example/madpark/MapsActivity.java
@@ -33,7 +33,6 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
